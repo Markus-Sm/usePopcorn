d556d0a08421c78ce68140387ab21a3dd19df6d8
Marek Samofal <marek.samofal@gmail.com>
2024-04-05 18:00:48 +0200

    docs: update

--- src/index.js
+++ src/index.js
@@ -25,6 +25,7 @@ root.render(
 	<React.StrictMode>
 		{/* <App /> */}
 		<StarRating maxRating={5} messages={['Terrible', 'Bad', 'Okay', 'Good', 'Awsome']} />
+		<StarRating color='brown' />
 
 		<Test />
 	</React.StrictMode>
