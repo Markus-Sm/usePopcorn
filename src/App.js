28f5ffbc55d95b3f511f5c8055d1d164541fea74
Marek Samofal <marek.samofal@gmail.com>
2024-04-25 16:49:14 +0200

    Useing an async function

--- src/App.js
+++ src/App.js
@@ -52,11 +52,17 @@ const KEY = 'c218c321'
 export default function App() {
 	const [movies, setMovies] = useState(tempMovieData)
 	const [watched, setWatched] = useState(tempWatchedData)
+	const query = 'interstellar'
 
 	useEffect(function () {
-		fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
-			.then(res => res.json())
-			.then(data => setMovies(data.Search))
+		async function fetchMovies() {
+			const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`)
+			const data = await res.json()
+			setMovies(data.Search)
+			console.log(data.Search)
+		}
+
+		fetchMovies()
 	}, [])
 
 	return (
