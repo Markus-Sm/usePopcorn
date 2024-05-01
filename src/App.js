9d037767994b06d5945ade3d5fdf205160d3ac7c
Marek Samofal <marek.samofal@gmail.com>
2024-05-01 16:34:46 +0200

    Loading movie details 01

--- src/App.js
+++ src/App.js
@@ -244,6 +244,19 @@ function Movie({ movie, onSelectMovie }) {
 }
 
 function MovieDetails({ selectedId, onCloseMovie }) {
+	const [movie, setMovie] = useState({})
+
+	useEffect(function () {
+		async function getMovieDetails() {
+			const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`)
+			const data = await res.json()
+			setMovie(data)
+			console.log(data)
+		}
+
+		getMovieDetails()
+	}, [])
+
 	return (
 		<div className='details'>
 			<button className='btn-back' onClick={onCloseMovie}>
